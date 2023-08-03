6f513b8411ca9fc0aaf268c133c8d742d00f293e
Vivek Sharma <vivek@denovogroup.org>
2023-08-03 16:04:10 +0530

    add all

---
+++ project5/secondapp/imports/ui/components/brand/Brand.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './brand.css'
+
+const Brand = () =>{
+    return(
+        <div className="brand"></div>
+        )
+}
+
+export default Brand
---
+++ project5/secondapp/imports/ui/containers/whatGPT3/WhatGPT3.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './whatGPT3.css'
+
+const whatGPT3 = () =>{
+    return(
+        <div className="what-gpt">
+
+        </div>
+    )
+}
+
+export default whatGPT3;
